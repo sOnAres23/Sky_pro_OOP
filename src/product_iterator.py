a67ddd1619d40cc21e13ea8d4f887df3ed19d621
Sergey Spisak <serega232003@gmail.com>
2024-08-07 17:06:53 +0300

    h/w 16.1 done

--- src/product_iterator.py
+++ src/product_iterator.py
@@ -1,38 +1,38 @@
-# from src.product import Product
-# from src.category import Category
-#
-#
-# class ProductIterator:
-#     """Класс для итерации продуктов класса Category"""
-#
-#     def __init__(self, category_obj: object):
-#         self.category = category_obj
-#         self.index = 0
-#
-#     def __iter__(self) -> object:
-#         self.index = 0
-#         return self
-#
-#     def __next__(self) -> object:
-#         if self.index < len(self.category.get_products_list):
-#             product = self.category.get_products_list[self.index]
-#             self.index += 1
-#             return product
-#         else:
-#             raise StopIteration
-#
-#
-# product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-# product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-# product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-#
-# category1 = Category(
-#         "Смартфоны",
-#         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-#         [product1, product2, product3]
-#     )
-#
-# iterator = ProductIterator(category1)
-#
-# for item in iterator:
-#     print(item)
+from src.product import Product
+from src.category import Category
+
+
+class ProductIterator:
+    """Класс для итерации продуктов класса Category"""
+
+    def __init__(self, category_obj: Category):
+        self.category = category_obj
+        self.index = 0
+
+    def __iter__(self) -> object:
+        self.index = 0
+        return self
+
+    def __next__(self) -> object:
+        if self.index < len(self.category.products):
+            product = self.category.products[self.index]
+            self.index += 1
+            return product
+        else:
+            raise StopIteration
+
+
+product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3]
+    )
+
+iterator = ProductIterator(category1)
+
+for item in iterator:
+    print(item)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,27 @@
 import pytest
 
 from src.category import Category
+from src.lawngrass import LawnGrass
 from src.product import Product
-# from src.product_iterator import ProductIterator
+from src.smartphone import Smartphone
+
+from src.product_iterator import ProductIterator
 
 
 @pytest.fixture
 def first_product():
-    return Product(name="Samsung Galaxy S23 Ultra",
-                   description="256GB, Серый цвет, 200MP камера",
-                   price=180000.0,
-                   quantity=5)
+    return Product("Samsung Galaxy S23 Ultra",
+                   "256GB, Серый цвет, 200MP камера",
+                   180000.0,
+                   5)
 
 
 @pytest.fixture
 def second_product():
-    return Product(name="Iphone 15",
-                   description="512GB, Gray space",
-                   price=210000.0,
-                   quantity=8)
+    return Product("Iphone 15",
+                   "512GB, Gray space",
+                   210000.0,
+                   8)
 
 
 @pytest.fixture
@@ -37,7 +40,7 @@ def first_category():
 def second_category():
     return Category(name="Телевизоры",
                     description="Современный телевизор, который "
-                         "позволяет наслаждаться просмотром, станет вашим другом и помощником",
+                                "позволяет наслаждаться просмотром, станет вашим другом и помощником",
                     products=[Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)])
 
 
@@ -52,5 +55,29 @@ def product_dict():
 
 
 @pytest.fixture
+def first_smartphone():
+    return Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
+                      "S23 Ultra", 256, "Серый")
+
+
+@pytest.fixture
+def second_smartphone():
+    return Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2,
+                      "15", 512, "Gray space")
+
+
+@pytest.fixture
+def first_lawngrass():
+    return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20,
+                     "Россия", "7 дней", "Зеленый")
+
+
+@pytest.fixture
+def second_lawngrass():
+    return LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15,
+                     "США", "5 дней", "Темно-зеленый")
+
+
+@pytest.fixture
 def product_iterator(first_category):
-    return ProductIterator(first_product)
+    return ProductIterator(first_category)
